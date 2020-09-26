3b77a80001c287a5f861a53438c28e94b762f0eb
hoslo <ys952572463@163.com>
2020-09-26 11:14:05 +0800

    FIRST

---
+++ gee.go
@@ -0,0 +1,92 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+type HandleFunc func(*Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix string
+
+	middlewares []HandleFunc
+
+	parent *RouterGroup
+
+	engine *Engine
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRouter(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandleFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandleFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) Use(middlewares ...HandleFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandleFunc) {
+	e.router.addRouter(method, pattern, handler)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandleFunc
+	for _, group := range e.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(w, req)
+	c.handlers = middlewares
+	e.router.handle(c)
+}
+
+func (e *Engine) GET(pattern string, handler HandleFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+func (e *Engine) POST(pattern string, handler HandleFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
+}
